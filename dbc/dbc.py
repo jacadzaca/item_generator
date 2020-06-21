2905895a9cabef7f00a4f4e1832296ea1d928c27
jacadzaca <vitouejj@gmail.com>
2020-06-21 16:31:30 +0200

    add functions for manipulating dbc files

---
+++ dbc/dbc.py
@@ -0,0 +1,18 @@
+def append_record(record, header, file):
+    '''
+    file MUST have write permissions
+    this method changes the file's position
+    '''
+    # increment record_count
+    header.record_count += 1
+    file.seek(0)
+    for field in header.to_bytes():
+        file.write(field)
+
+    file.seek(header.size + (header.record_count - 1) * header.record_size)
+    for field in record.to_bytes():
+        file.write(field)
+
+
+def find(entry, records):
+    return next(filter(lambda item: item.entry == entry, records))
