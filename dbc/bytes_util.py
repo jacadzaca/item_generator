ece7464e93d61a8e3c4cc74a8fe3fa7fbcb99d8d
jacadzaca <vitouejj@gmail.com>
2020-06-21 16:30:15 +0200

    add bytes_util.py

---
+++ dbc/bytes_util.py
@@ -0,0 +1,11 @@
+'''
+contains helper functions for conversions to/from byte representations
+'''
+
+
+def to_int(byttes):
+    return int.from_bytes(byttes, byteorder='little', signed=True)
+
+
+def to_bytes(intt, field_size):
+    return intt.to_bytes(field_size, byteorder='little', signed=True)
