a98c4dbf169b0c89a4134cbca8081cd3aecaf000
jacadzaca <vitouejj@gmail.com>
2020-06-23 18:54:52 +0200

    changed DBCFile.records to public

--- dbc/dbc_file.py
+++ dbc/dbc_file.py
@@ -12,8 +12,8 @@ class DBCFile():
     def __init__(self, file_descriptor, template_entry):
         self._f = file_descriptor
         self._header = DBCHeader(self._f)
-        self._records = RecordIterator(self._f, self._header)
-        self._template = dbc.edit.find(template_entry, self._records)
+        self.records = RecordIterator(self._f, self._header)
+        self._template = dbc.edit.find(template_entry, self.records)
 
     def add_record(self, entry, display_id):
         template = copy.deepcopy(self._template)
