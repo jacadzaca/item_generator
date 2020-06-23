b5637c4ef0c62295dc282181f514af65f049726e
jacadzaca <vitouejj@gmail.com>
2020-06-23 19:45:49 +0200

    add type hitns to dbc/bytes_util

--- dbc/bytes_util.py
+++ dbc/bytes_util.py
@@ -3,9 +3,9 @@ contains helper functions for conversions to/from byte representations
 '''
 
 
-def to_int(byttes):
+def to_int(byttes: list):
     return int.from_bytes(byttes, byteorder='little', signed=True)
 
 
-def to_bytes(intt, field_size):
+def to_bytes(intt: int, field_size: int):
     return intt.to_bytes(field_size, byteorder='little', signed=True)
