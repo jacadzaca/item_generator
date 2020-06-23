84481e3065acb4d013364ac1623d77826bcaf13b
jacadzaca <vitouejj@gmail.com>
2020-06-23 19:10:12 +0200

    added class method for DBCFile

--- dbc/dbc_file.py
+++ dbc/dbc_file.py
@@ -9,10 +9,10 @@ class DBCFile():
     file_descriptor MUST be opened in a rb+ mode
     '''
 
-    def __init__(self, file_descriptor, template_entry):
-        self._f = file_descriptor
-        self._header = DBCHeader(self._f)
-        self.records = RecordIterator(self._f, self._header)
+    def __init__(self, file_handle, header, records, template_entry):
+        self._f = file_handle
+        self._header = header
+        self.records = records
         self._template = dbc.edit.find(template_entry, self.records)
 
     def add_record(self, entry, display_id):
@@ -20,3 +20,10 @@ class DBCFile():
         template.entry = entry
         template.display_id = display_id
         dbc.edit.append_record(template, self._header, self._f)
+
+    @classmethod
+    def from_file_handle(cls, file_handle, record_creator, template_entry):
+        header = DBCHeader(file_handle)
+        records = RecordIterator(file_handle, header, record_creator)
+        template = dbc.edit.find(template_entry, records)
+        return cls(file_handle, header, records, template)
