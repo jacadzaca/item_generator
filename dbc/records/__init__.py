811cefb8532625acbbe415a416276d723d7acb6e
jacadzaca <vitouejj@gmail.com>
2020-06-24 11:53:25 +0200

    add info about to_bytes() to docstring

--- dbc/records/__init__.py
+++ dbc/records/__init__.py
@@ -1,8 +1,9 @@
 """
 stores different representations of dbc file structures
-ALL representations should specify a strings method
+ALL representations MUST specify a strings() method
 that returns string to be put in string_block section,
 if no strings are to be put, the implementation should return NO_STRING.
+ALL representations MUST specify a to to_bytes() method
 for different types of records @see https://wowdev.wiki/Category:DBC_WotLK
 """
 
