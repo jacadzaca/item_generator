4bb048cd74243443c503952ddcea158ae0afe2d5
jacadzaca <vitouejj@gmail.com>
2020-06-24 11:55:32 +0200

    move comment to docstring

--- stat_type.py
+++ stat_type.py
@@ -1,8 +1,11 @@
 import enum
 
+"""
+https://trinitycore.atlassian.net/wiki/spaces/tc/pages/2130222/item+template#item_template-stat_type
+"""
+
 
 class StatType(enum.Enum):
-    # https://trinitycore.atlassian.net/wiki/spaces/tc/pages/2130222/item+template#item_template-stat_type
     AGILITY = 3
     STRENGTH = 4
     INTELLECT = 5
